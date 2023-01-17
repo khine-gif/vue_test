cf5aab30a5f3b8dc4fe9e659cb8751c5f26ff3b5
Khine <73374265+khine-gif@users.noreply.github.com>
2023-01-17 16:01:23 +0630

    create vue in wp

---
+++ home.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Template Name: Home Page Template
+*/
+/**
+ * @package v_1.0.1
+ */
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+
+		This is Home
+
+	</main><!-- #main -->
+
+<?php
+get_footer();
